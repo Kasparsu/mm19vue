21b53f114fbed9a72a6c18f93a200a3ca9333742
kaspar suursalu <kaspar.suursalu@tptlive.ee>
2020-09-18 11:34:45 +0300

    add axios example

---
+++ JsonExample.vue
@@ -0,0 +1,24 @@
+<template>
+    <joke></joke>
+</template>
+
+<script>
+import Joke from './Joke.vue';
+export default {
+    components: {
+        joke: Joke
+    },
+    created(){
+      
+    },
+    data(){
+        return {
+           
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
