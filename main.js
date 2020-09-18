dad82e771ec23afed53c0ac811211d036310435d
kaspar suursalu <kaspar.suursalu@tptlive.ee>
2020-09-18 10:13:24 +0300

    add tabs example component

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ import Vue from 'vue/dist/vue.common.js';
 
 import ToDo from './ToDo.vue';
 import ModalExample from './ModalExample.vue';
+import TabsExample from './TabsExample.vue';
 Vue.component('to-do', ToDo);
 Vue.component('modal-example', ModalExample);
-
+Vue.component('tabs-example', TabsExample);
 let App = new Vue({
     el: '#app',
 })
