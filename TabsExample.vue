0a2c102e50a9b4ec78e57a7fb1837a640e6c342d
kaspar suursalu <kaspar.suursalu@tptlive.ee>
2020-09-18 10:38:04 +0300

    add Tabs component

--- TabsExample.vue
+++ TabsExample.vue
@@ -1,10 +1,18 @@
 <template>
-  
+    <tabs :titles="headers"></tabs>
 </template>
 
 <script>
+import Tabs from './Tabs.vue';
 export default {
-
+    components: {
+        tabs: Tabs
+    }, 
+    data() {
+        return {
+            headers: ['Games', 'Books', 'Shows', 'Music'],
+        }
+    }
 }
 </script>
 
