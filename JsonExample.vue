3185a1575c5d9cad32d3ad09b4b8830deddb22b0
kaspar suursalu <kaspar.suursalu@tptlive.ee>
2020-10-02 09:14:45 +0300

    DRY code

--- JsonExample.vue
+++ JsonExample.vue
@@ -1,14 +1,12 @@
 <template>
   <div>
     <search></search>
-    <button class="button" :disabled="!pagination.next" @click="next">Next</button>
-    <button class="button" :disabled="!pagination.prev" @click="prev">Prev</button>
+    <pagination :info="pagination" :currentpage="1"></pagination>
     <div class="columns is-multiline">
       <div class="column is-one-quarter" v-for="character in characters" :key="character.id">
         <card :character="character"></card>
       </div>
     </div>
-
   </div>
 
 
@@ -19,19 +17,17 @@ import Joke from './Joke.vue';
 import Search from "./Search";
 import Card from "./Card";
 import axios from "axios";
+import Pagination from "./Pagination";
 export default {
     components: {
+      Pagination,
       Card,
       Search,
         joke: Joke,
 
     },
     created(){
-      axios.get('https://rickandmortyapi.com/api/character/').then(response => {
-        console.log(response.data);
-        this.characters = response.data.results;
-        this.pagination = response.data.info;
-      });
+      this.getPage('https://rickandmortyapi.com/api/character/');
     },
     data(){
         return {
@@ -41,15 +37,13 @@ export default {
     },
     methods: {
       next(){
-        axios.get(this.pagination.next).then(response => {
-          console.log(response.data);
-          this.characters = response.data.results;
-          this.pagination = response.data.info;
-        });
-
+        this.getPage(this.pagination.next);
       },
-      prev() {
-        axios.get(this.pagination.prev).then(response => {
+      prev(){
+        this.getPage(this.pagination.prev);
+      },
+      getPage(url){
+        axios.get(url).then(response => {
           console.log(response.data);
           this.characters = response.data.results;
           this.pagination = response.data.info;
