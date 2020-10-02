eb14d0ac20c422b9d2134768ab965222c165d477
kaspar suursalu <kaspar.suursalu@tptlive.ee>
2020-10-02 08:21:37 +0300

    add card pagination

---
+++ Card.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="card">
+    <div class="card-image">
+      <figure class="image is-4by3">
+        <img :src="character.image" alt="Placeholder image">
+      </figure>
+    </div>
+    <div class="card-content">
+      <div class="content">
+        <ul>
+          <li>Name: {{character.name}}</li>
+          <li>Gender: {{character.gender}}</li>
+          <li>Species: {{character.species}}</li>
+          <li>Origin: {{character.origin.name}}</li>
+          <li>Location: {{character.location.name}}</li>
+          <li>Status: {{character.status}}</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+name: "Card",
+  props: ['character']
+}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+}
+</style>
--- JsonExample.vue
+++ JsonExample.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
     <search></search>
+    <button class="button" :disabled="!pagination.next" @click="next">Next</button>
+    <button class="button" :disabled="!pagination.prev" @click="prev">Prev</button>
+    <div class="columns is-multiline">
+      <div class="column is-one-quarter" v-for="character in characters" :key="character.id">
+        <card :character="character"></card>
+      </div>
+    </div>
+
   </div>
 
 
@@ -9,18 +17,44 @@
 <script>
 import Joke from './Joke.vue';
 import Search from "./Search";
+import Card from "./Card";
+import axios from "axios";
 export default {
     components: {
+      Card,
       Search,
-        joke: Joke
+        joke: Joke,
+
     },
     created(){
-      
+      axios.get('https://rickandmortyapi.com/api/character/').then(response => {
+        console.log(response.data);
+        this.characters = response.data.results;
+        this.pagination = response.data.info;
+      });
     },
     data(){
         return {
-           
+           characters: null,
+           pagination: null
         }
+    },
+    methods: {
+      next(){
+        axios.get(this.pagination.next).then(response => {
+          console.log(response.data);
+          this.characters = response.data.results;
+          this.pagination = response.data.info;
+        });
+
+      },
+      prev() {
+        axios.get(this.pagination.prev).then(response => {
+          console.log(response.data);
+          this.characters = response.data.results;
+          this.pagination = response.data.info;
+        });
+      }
     }
 }
 </script>
